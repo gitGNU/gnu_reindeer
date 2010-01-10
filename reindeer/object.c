0a5a41a822c1d63027348c03046f325c158d07b7
Patrik Olsson <peo@xaci.be>
2010-01-10 19:55:54 +0100

    Add 2010 to copyright years

--- reindeer/object.c
+++ reindeer/object.c
@@ -1,7 +1,7 @@
 /*
     This file is part of Reindeer.
 
-    Copyright (C) 2008, 2009 - Patrik Olsson
+    Copyright (C) 2008, 2009, 2010 - Patrik Olsson
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
